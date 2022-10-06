a24eba8f5b024f037186da01e19349d09e832c87
Nick <nmcarducci@gmail.com>
2022-10-06 13:30:55 -0400

    ok

--- pm2.js
+++ pm2.js
@@ -1,15 +1,17 @@
 const app = require("./src/index.js");
 //ecosystem.config.cjs can require this (i.e. ex-"Must use import to load ES Module")
-module.exports = {
-  "name": "mastercard-backbank-digital-ocean",
-  "interpreter": "node",
-  "script": app,
-  "merge_logs": true,
-  "cwd": "./src",
-  "env_development": {
-    "NODE_ENV": "development"
-  },
-  "env": {
-    "NODE_ENV": "production"
+module.exports = function app() {
+  return {
+    "name": "mastercard-backbank-digital-ocean",
+    "interpreter": "node",
+    "script": app,
+    "merge_logs": true,
+    "cwd": "./src",
+    "env_development": {
+      "NODE_ENV": "development"
+    },
+    "env": {
+      "NODE_ENV": "production"
+    }
   }
 }
