9859146de24719b78282f8a93527c5dc23ad385c
Nick <nmcarducci@gmail.com>
2022-10-06 13:39:10 -0400

    ok

--- pm2.js
+++ pm2.js
@@ -1,6 +1,6 @@
 //const app = require("./src/index.js");
 //ecosystem.config.cjs can require this (i.e. ex-"Must use import to load ES Module")
-export default () => {
+module.exports = () => {
   return {
     "name": "mastercard-backbank-digital-ocean",
     "interpreter": "node",
