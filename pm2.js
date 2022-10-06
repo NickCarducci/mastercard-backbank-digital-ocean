9a738bd3188d82986652bda29ed3bffabd9169e5
Nick <nmcarducci@gmail.com>
2022-10-06 13:10:48 -0400

    ok

---
+++ pm2.js
@@ -0,0 +1,15 @@
+import app from "src/index.mjs";
+//ecosystem.config.cjs can require this (i.e. ex-"Must use import to load ES Module")
+export default {
+  "name": "mastercard-backbank-digital-ocean",
+  "interpreter": "node",
+  "script": app,
+  "merge_logs": true,
+  "cwd": "./src",
+  "env_development": {
+    "NODE_ENV": "development"
+  },
+  "env": {
+    "NODE_ENV": "production"
+  }
+}
