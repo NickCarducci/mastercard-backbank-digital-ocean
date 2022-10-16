8fddb56c8295fb5dffd27aa0b1410adaebc6bbe2
Nick <nmcarducci@gmail.com>
2022-10-16 00:04:46 -0400

    ok

--- src/index.js
+++ src/index.js
@@ -24,96 +24,6 @@ const express = require('express');
     return UseDependency();
 }*/
 
-function UseDependency() {
-    // Script-Name: atm_locations
-    var MasterCardAPI = locations.MasterCardAPI;
-    //var consumerKey = "your consumer key";   // You should copy this from "My Keys" on your project page e.g. UTfbhDCSeNYvJpLL5l028sWL9it739PYh6LU5lZja15xcRpY!fd209e6c579dc9d7be52da93d35ae6b6c167c174690b72fa
-    var keyStorePath = process.env.p12;// service worker envar "path to your .p12 private key file"; // e.g. /Users/yourname/project/sandbox.p12 | C:\Users\yourname\project\sandbox.p12
-    var keyAlias = "keyalias";   // For production: change this to the key alias you chose when you created your production key
-    var keyPassword = "keystorepassword";   // For production: change this to the key alias you chose when you created your production key
-    // You only need to do initialize MasterCardAPI once
-    var authentication = new MasterCardAPI.OAuth(process.env.consumerKey, keyStorePath, keyAlias, keyPassword);
-    MasterCardAPI.init({
-        sandbox: true,
-        debug: true,
-        authentication: authentication
-    });
-    var requestData = {
-        "PageOffset": "0",
-        "PageLength": "5",
-        "PostalCode": "11101"
-    };
-    locations.ATMLocations.query(requestData
-        , function (error, data) {
-            if (error) {
-                err("HttpStatus: " + error.getHttpStatus());
-                err("Message: " + error.getMessage());
-                err("ReasonCode: " + error.getReasonCode());
-                err("Source: " + error.getSource());
-                err(error);
-            }
-            else {
-                out(data.Atms.PageOffset); //-->0
-                out(data.Atms.TotalCount); //-->26
-                out(data.Atms.Atm[0].Location.Name); //-->Sandbox ATM Location 1
-                out(data.Atms.Atm[0].Location.Distance); //-->0.93
-                out(data.Atms.Atm[0].Location.DistanceUnit); //-->MILE
-                out(data.Atms.Atm[0].Location.Address.Line1); //-->4201 Leverton Cove Road
-                out(data.Atms.Atm[0].Location.Address.Line2); //-->
-                out(data.Atms.Atm[0].Location.Address.City); //-->SPRINGFIELD
-                out(data.Atms.Atm[0].Location.Address.PostalCode); //-->11101
-                out(data.Atms.Atm[0].Location.Address.CountrySubdivision.Name); //-->UYQQQQ
-                out(data.Atms.Atm[0].Location.Address.CountrySubdivision.Code); //-->QQ
-                out(data.Atms.Atm[0].Location.Address.Country.Name); //-->UYQQQRR
-                out(data.Atms.Atm[0].Location.Address.Country.Code); //-->UYQ
-                out(data.Atms.Atm[0].Location.Point.Latitude); //-->38.76006576913497
-                out(data.Atms.Atm[0].Location.Point.Longitude); //-->-90.74615107952418
-                out(data.Atms.Atm[0].Location.LocationType.Type); //-->OTHER
-                out(data.Atms.Atm[0].HandicapAccessible); //-->NO
-                out(data.Atms.Atm[0].Camera); //-->NO
-                out(data.Atms.Atm[0].Availability); //-->UNKNOWN
-                out(data.Atms.Atm[0].AccessFees); //-->UNKNOWN
-                out(data.Atms.Atm[0].Owner); //-->Sandbox ATM 1
-                out(data.Atms.Atm[0].SharedDeposit); //-->NO
-                out(data.Atms.Atm[0].SurchargeFreeAlliance); //-->NO
-                out(data.Atms.Atm[0].SurchargeFreeAllianceNetwork); //-->DOES_NOT_PARTICIPATE_IN_SFA
-                out(data.Atms.Atm[0].Sponsor); //-->Sandbox
-                out(data.Atms.Atm[0].SupportEMV); //-->1
-                out(data.Atms.Atm[0].InternationalMaestroAccepted); //-->1
-                //This sample shows looping through Atms.Atm
-                console.log("This sample shows looping through Atms.Atm");
-                data.Atms.Atm.forEach(function (item) {
-                    outObj(item, "Location")
-                    outObj(item, "HandicapAccessible")
-                    outObj(item, "Camera")
-                    outObj(item, "Availability")
-                    outObj(item, "AccessFees")
-                    outObj(item, "Owner")
-                    outObj(item, "SharedDeposit")
-                    outObj(item, "SurchargeFreeAlliance")
-                    outObj(item, "SurchargeFreeAllianceNetwork")
-                    outObj(item, "Sponsor")
-                    outObj(item, "SupportEMV")
-                    outObj(item, "InternationalMaestroAccepted")
-                });
-            }
-        });
-
-    var output = []
-    function out(value) {
-        output.push(value);
-    }
-
-    function outObj(item, key) {
-        output.push(item[key]);
-    }
-
-    function err(value) {
-        output.push(value);
-    }
-    return out(output);
-}//so called invitation
-
 const app = express();
 const port = 8080;
 //http://johnzhang.io/options-request-in-express
@@ -133,26 +43,54 @@ app/*.use((_req, _res, next) => {
     //res.send(200,"ok")
     next()
 })*/.get('/', (req, res) => res.status(200).send("shove it"))
-//https://stackoverflow.com/questions/36554375/getting-the-request-origin-in-express
-.options("/", (req, res) => {
-    var origin = req.headers.origin;
-    if (allowedOrigins.indexOf(origin) === -1) return res.status(401).send(`{error:${"no access for this origin- " + origin}}`);
-    //res.header("":_)
-    res.set("Access-Control-Allow-Methods", "POST, OPTIONS, GET");
-    res.set("Access-Control-Allow-Origin", allowedOrigins[allowedOrigins.indexOf(origin)]);
-    res.set("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Methods, Origin, Content-Type, Referer, Accept");
-    res.set("Content-Type", "Application/JSON");
-    //res.send(200,"ok")
-    res.status(204).send({data:"ok"});
-    //res.sendStatus(204);
-})
-.post('/', (req, res) => {
-    //if (request.method === "OPTIONS")return res.send(`preflight response for POST`);
-    //res.set("Content-Type", "Application/JSON");
-    var origin = req.headers.origin;
-    res.set("Access-Control-Allow-Origin", origin);
-    //res.status(200).send({data:"ok"});
-    res.status(200).send({data:UseDependency()});
-})
-.listen(port, () => console.log(`localhost:${port}`));
+    //https://stackoverflow.com/questions/36554375/getting-the-request-origin-in-express
+    .options("/", (req, res) => {
+        var origin = req.headers.origin;
+        if (allowedOrigins.indexOf(origin) === -1) return res.status(401).send(`{error:${"no access for this origin- " + origin}}`);
+        //res.header("":_)
+        res.set("Access-Control-Allow-Methods", "POST, OPTIONS, GET");
+        res.set("Access-Control-Allow-Origin", allowedOrigins[allowedOrigins.indexOf(origin)]);
+        res.set("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Methods, Origin, Content-Type, Referer, Accept");
+        res.set("Content-Type", "Application/JSON");
+        //res.send(200,"ok")
+        res.status(204).send({ data: "ok" });
+        //res.sendStatus(204);
+    })
+    .post('/', (req, res) => {
+        //if (request.method === "OPTIONS")return res.send(`preflight response for POST`);
+        //res.set("Content-Type", "Application/JSON");
+        var origin = req.headers.origin;
+        res.set("Access-Control-Allow-Origin", origin);
+        //res.status(200).send({data:"ok"});
+
+        var MasterCardAPI = locations.MasterCardAPI;
+        //var consumerKey = "your consumer key";   // You should copy this from "My Keys" on your project page e.g. UTfbhDCSeNYvJpLL5l028sWL9it739PYh6LU5lZja15xcRpY!fd209e6c579dc9d7be52da93d35ae6b6c167c174690b72fa
+        var keyStorePath = process.env.p12;// service worker envar "path to your .p12 private key file"; // e.g. /Users/yourname/project/sandbox.p12 | C:\Users\yourname\project\sandbox.p12
+        var keyAlias = "keyalias";   // For production: change this to the key alias you chose when you created your production key
+        var keyPassword = "keystorepassword";   // For production: change this to the key alias you chose when you created your production key
+        MasterCardAPI.init({
+            sandbox: true,
+            debug: true,
+            authentication: new MasterCardAPI.OAuth(process.env.consumerKey, keyStorePath, keyAlias, keyPassword)
+        });
+        var requestData = {
+            "PageOffset": "0",
+            "PageLength": "5",
+            "PostalCode": "11101"
+        };
+        locations.ATMLocations.query(
+            requestData, (error, data) => {
+                if (error) {
+                    err("HttpStatus: " + error.getHttpStatus());
+                    err("Message: " + error.getMessage());
+                    err("ReasonCode: " + error.getReasonCode());
+                    err("Source: " + error.getSource());
+                    err(error);
+                }
+            }
+        );
+
+        res.status(200).send({ data: UseDependency() });
+    })
+    .listen(port, () => console.log(`localhost:${port}`));
 
