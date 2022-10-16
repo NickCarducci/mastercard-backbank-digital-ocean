d7ddb304eb6f41a30414e7c4821dec699ce6d7b7
Nick <nmcarducci@gmail.com>
2022-10-15 23:43:37 -0400

    ok

--- src/index.js
+++ src/index.js
@@ -28,11 +28,11 @@ function UseDependency() {
     // Script-Name: atm_locations
     var MasterCardAPI = locations.MasterCardAPI;
     //var consumerKey = "your consumer key";   // You should copy this from "My Keys" on your project page e.g. UTfbhDCSeNYvJpLL5l028sWL9it739PYh6LU5lZja15xcRpY!fd209e6c579dc9d7be52da93d35ae6b6c167c174690b72fa
-    var keyStorePath = P12;// service worker envar "path to your .p12 private key file"; // e.g. /Users/yourname/project/sandbox.p12 | C:\Users\yourname\project\sandbox.p12
+    var keyStorePath = process.env.p12;// service worker envar "path to your .p12 private key file"; // e.g. /Users/yourname/project/sandbox.p12 | C:\Users\yourname\project\sandbox.p12
     var keyAlias = "keyalias";   // For production: change this to the key alias you chose when you created your production key
     var keyPassword = "keystorepassword";   // For production: change this to the key alias you chose when you created your production key
     // You only need to do initialize MasterCardAPI once
-    var authentication = new MasterCardAPI.OAuth(consumerKey, keyStorePath, keyAlias, keyPassword);
+    var authentication = new MasterCardAPI.OAuth(process.env.consumerKey, keyStorePath, keyAlias, keyPassword);
     MasterCardAPI.init({
         sandbox: true,
         debug: true,
